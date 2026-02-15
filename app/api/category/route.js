0e4c79ddcb94431a183b79c200ce064e52f3733e
naveenrapelly27 <naveenrapelly2705@gmail.com>
2026-02-15 20:03:46 +0530

    fix: prevent api prerender

--- app/api/category/route.js
+++ app/api/category/route.js
@@ -1,3 +1,6 @@
+export const dynamic = "force-dynamic";
+export const runtime = "nodejs";
+
 import { ConnectDB } from "@/lib/config/db";
 import BlogModel from "@/lib/models/BlogModel";
 import { NextResponse } from "next/server";
@@ -6,7 +9,6 @@ export async function GET() {
   await ConnectDB();
 
   const blogs = await BlogModel.find({}, "category");
-
   const categories = [...new Set(blogs.map(b => b.category))];
 
   return NextResponse.json({ categories });
